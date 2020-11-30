a80d49fbf037227af8962310c2c53403fb0497bb
Ryuusakii <ryuusakis@gmail.com>
2020-11-30 17:05:30 -0300

    Remove preloader

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,6 @@
 "use strict";
 
 
-// Prealoder
-function prealoader() {
-  if ($('#preloader_1').length) {
-    $('#preloader_1').fadeOut(); // will first fade out the loading animation
-    $('#loader-wrapper').delay(40).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(40).css({
-      'overflow': 'visible'
-    });
-  };
-}
-
 
 // placeholder remove
 function removePlaceholder() {
